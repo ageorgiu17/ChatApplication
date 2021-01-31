b654ca6e38af94291adb0411df17552df39a7805
Andrei Georgiu <georgiuandrei05@gmail.com>
2021-01-31 17:17:26 +0200

    Accepting multiple connections

--- ChatApplication/src/main/java/ServerMain.java
+++ ChatApplication/src/main/java/ServerMain.java
@@ -13,6 +13,10 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
+import java.lang.Thread;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerMain {
     public static void main(String[] args){
@@ -20,16 +24,38 @@ public class ServerMain {
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             while(true){
+                System.out.println("About to accept the connection....");
                 Socket clientSocket = serverSocket.accept();
-                OutputStream outputStream = clientSocket.getOutputStream();
-                outputStream.write("Hello World\n".getBytes());
-                clientSocket.close();
-                
+                System.out.println("Accepted connection from " + clientSocket);
+                Thread t;
+                t = new Thread(){
+                    @Override
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        } catch (InterruptedException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
         
+    }
+
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for(int i = 0; i < 10; i ++){
+            
+            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        clientSocket.close();
     }
     
 }
