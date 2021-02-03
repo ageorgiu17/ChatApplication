a803a3bda78739e79431e646ca7688f56e1572e3
Andrei Georgiu <georgiuandrei05@gmail.com>
2021-02-03 12:28:04 +0200

    Adding user Online/Offline API Methods

--- ChatApplication/src/main/java/ChatClient.java
+++ ChatApplication/src/main/java/ChatClient.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -24,6 +26,8 @@ public class ChatClient {
     private InputStream serverIn;
     private BufferedReader bufferIn;
     
+    private ArrayList<UserStatusListener> userStatusListener = new ArrayList<>();
+    
     public ChatClient(String serverName, int serverPort) {
         this.serverName = serverName;
         this.serverPort = serverPort;
@@ -31,6 +35,18 @@ public class ChatClient {
     
     public static void main(String[] args) throws IOException {
         ChatClient client = new ChatClient("localhost", 8818);
+        client.addUserStatusListener(new UserStatusListener(){
+            @Override
+            public void online(String login) {
+                System.out.println("ONLINE: " + login);
+            }
+
+            @Override
+            public void offline(String login) {
+                System.out.println("OFFLINE: " + login);
+            }
+    });
+    
         if(!client.connect()){
             System.err.println("Connection failed!");
         }else {
@@ -40,6 +56,8 @@ public class ChatClient {
             }else {
                 System.err.println("Error Login!");
             }
+            
+            client.logoff();
             
         }
     }
@@ -66,10 +84,73 @@ public class ChatClient {
         System.out.println("Response Line: " + response);
         
         if("Login Succescful".equalsIgnoreCase(response)){
+            startMessageReader();
             return true;
         } else {
             return false;
         }
         
+    }
+    
+    public void addUserStatusListener(UserStatusListener listener){
+        userStatusListener.add(listener);
+    }
+    public void removeUserStatusListener(UserStatusListener listener){
+        userStatusListener.remove(listener);
+    }
+
+    private void startMessageReader() {
+        Thread t = new Thread(){
+            @Override
+            public void run(){
+                readMessageLoop();
+            }
+
+            
+        };
+        t.start();
+    }
+    
+    private void readMessageLoop(){
+        try {
+            String line;
+            while((line = bufferIn.readLine()) != null){
+                String[] tokens = StringUtils.split(line);
+                if(tokens != null && tokens.length > 0){
+                    String cmd = tokens[0];
+                    if("online".equalsIgnoreCase(cmd)){
+                        handleOnlineTokens(tokens);
+                    }else if("offline".equalsIgnoreCase(cmd)){
+                        handleOfflineTokens(tokens);
+                    }
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+            try{
+                socket.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleOnlineTokens(String[] tokens) {
+        String login = tokens[1];
+        for(UserStatusListener listener: userStatusListener){
+            listener.online(login);
+        }
+    }
+
+    private void handleOfflineTokens(String[] tokens) {
+        String login = tokens[1];
+        for(UserStatusListener listener: userStatusListener){
+            listener.offline(login);
+        }
+    }
+
+    private void logoff() throws IOException {
+        String cmd = "logoff!\n";
+        serverOut.write(cmd.getBytes());
     }
 }
